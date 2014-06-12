a6f6010b2dcf14870222a0a923a7a95666d4d513
DeepthiMondreti <deepthimondreti@gmail.com>
2014-06-12 12:33:01 -0700

    MVC Project
    Community Assist

---
+++ MvcProject/Controllers/AddressController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcProject.Models;
+
+namespace MvcProject.Controllers
+{
+    public class AddressController : Controller
+    {
+        private CommunityAssistEntities db = new CommunityAssistEntities();
+
+        //
+        // GET: /Address/
+
+
+        public ActionResult Index()
+        {
+            return View(db.People.ToList());
+        }
+
+        //
+        // GET: /Address/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Person person = db.People.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        //
+        // GET: /Address/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Address/Create
+
+        public ActionResult SearchIndex(string searchString)
+        {
+            var person = from m in db.People
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                person = person.Where(s => s.PersonLastName.Contains(searchString));
+            }
+
+            return View(person);
+        }
+
+        //
+        // POST: /Address/
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.People.Add(person);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
+
+        //
+        // GET: /Address/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Person person = db.People.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        //
+        // POST: /Address/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(person).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+
+        //
+        // GET: /Address/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Person person = db.People.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        //
+        // POST: /Address/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Person person = db.People.Find(id);
+            db.People.Remove(person);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
